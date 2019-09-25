4905731ee488543a08316d9879f607be9b270615
Ryan Supelana <ryanchester98@gmail.com>
2019-09-25 16:54:53 +0800

    Fix bug on key utils

--- reports_ms/db/sql/map/comments.repository.js
+++ reports_ms/db/sql/map/comments.repository.js
@@ -1,7 +1,7 @@
 const knex = require("../../knex");
 const Redis = require("ioredis");
 const redis = new Redis(process.env.REDIS_URL);
-let finder = require("../../../utilities").keys;
+let finder = require("../../../utils/keys");
 
 const Handler = {
   /*
